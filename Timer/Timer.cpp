f17235622c8d5106795d7da507a53703e076e56a
ppd <1326845559@qq.com>
2022-01-12 11:53:25 +0800

    利用大根堆实现超时定时器，超时自动关闭连接

---
+++ Timer/Timer.cpp
@@ -0,0 +1,64 @@
+//
+// Created by ppd on 2022/1/11.
+//
+#include "../Http/Httpconn.h"
+#include "Timer.h"
+
+/**
+ * 对连接添加超时定时器
+ * @param httpConn
+ * @param timeOut
+ * @param cb
+ */
+void Timer::add(std::shared_ptr<Httpconn> &httpConn, const TimeoutCallBack &cb) {
+    if(_ref.count(httpConn ) == 0) { //新节点
+        std::shared_ptr<TimerNode> timer(new TimerNode{Clock::now() + MS(_timeout),cb});
+        _ref.insert(std::make_pair(httpConn, timer));
+        _heap.push(timer);
+    }else {
+        std::shared_ptr<TimerNode> timer = _ref[httpConn];
+        timer->expires = Clock::now() + MS(_timeout);
+        timer->cb = cb;
+    }
+
+}
+/***
+ * 清除超时多余连接
+ */
+void Timer::tick() {
+    if(_heap.empty()) {
+       return;
+    }
+    while(!_heap.empty()) {
+        std::shared_ptr<TimerNode> timer = _heap.top();
+        if(std::chrono::duration_cast<MS>(timer->expires - Clock::now()).count() > 0) {
+            break;
+        }
+        timer->cb();
+        _heap.pop();
+    }
+}
+int Timer::GetNextTick() {
+    tick();
+    size_t res = -1;
+    if(!_heap.empty()) {
+        res = std::chrono::duration_cast<MS>(_heap.top()->expires - Clock::now()).count();
+        if(res < 0) { res = 0; }
+    }
+    return res;
+}
+
+Timer::~Timer() {
+    _ref.clear();
+
+}
+
+Timer::Timer(size_t timeout):_timeout(timeout){
+
+}
+
+void Timer::update(std::shared_ptr<Httpconn> &httpconn) {
+    std::shared_ptr<TimerNode> timer = _ref[httpconn];
+    timer->expires = Clock::now() + MS(_timeout);
+
+}
---
+++ Timer/Timer.h
@@ -0,0 +1,48 @@
+//
+// Created by ppd on 2022/1/11.
+//
+#pragma once
+#ifndef TCPSERVER_TIMER_H
+#define TCPSERVER_TIMER_H
+#include <unordered_map>
+#include <chrono>
+#include <functional>
+#include <queue>
+//#include "../Http/Httpconn.h"
+class Httpconn;
+
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds MS;
+typedef Clock::time_point TimeStamp;
+
+struct TimerNode {
+    TimeStamp expires;
+    TimeoutCallBack cb;
+};
+
+struct compare {
+    bool operator() (const std::shared_ptr<TimerNode> &ptr1,const std::shared_ptr<TimerNode> &ptr2) {
+        return ptr1->expires < ptr2->expires;
+
+    }
+};
+
+class Timer {
+public:
+    void add(std::shared_ptr<Httpconn> &httpconn, const TimeoutCallBack& cb);
+    void update(std::shared_ptr<Httpconn> &httpconn);
+    int GetNextTick();
+    void tick();
+    explicit Timer(size_t timeout);
+    ~Timer();
+
+private:
+    std::priority_queue<std::shared_ptr<TimerNode>,std::vector<std::shared_ptr<TimerNode>>,compare> _heap;
+    std::unordered_map<std::shared_ptr<Httpconn>,std::shared_ptr<TimerNode>> _ref;
+    size_t _timeout;
+
+};
+
+
+#endif //TCPSERVER_TIMER_H
